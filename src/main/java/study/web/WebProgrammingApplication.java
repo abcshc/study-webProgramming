79fba7af63145984b22eb978aa237c7e9e77c24f
abcshc <thrrk@naver.com>
2017-11-27 10:57:55 +0900

    initialize

---
+++ src/main/java/study/web/WebProgrammingApplication.java
@@ -0,0 +1,12 @@
+package study.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProgrammingApplication.class, args);
+	}
+}
---
+++ src/test/java/study/web/WebProgrammingApplicationTests.java
@@ -0,0 +1,16 @@
+package study.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebProgrammingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
